8b0a85305c7907994724f1da7a2877122bf31e42
taotaku <82632238+tao-taku@users.noreply.github.com>
2021-04-27 12:28:20 +0200

    decode unicode html entities on format

--- format.ts
+++ format.ts
@@ -1,10 +1,13 @@
 export function format(svg: string) {
-   return svg.replace(/&apos;/gmu, `'`).replace(
-      /^(  )+/gmu,
-      (indent) =>
-         indent
-            .match(/ {1,2}/gu)
-            ?.map(() => '\t')
-            .join('') ?? '',
-   );
+   return svg
+      .replace(/&apos;/gmu, `'`)
+      .replace(
+         /^(  )+/gmu,
+         (indent) =>
+            indent
+               .match(/ {1,2}/gu)
+               ?.map(() => '\t')
+               .join('') ?? '',
+      )
+      .replace(/&#x([0-9A-F]{4});/gmu, (_, codePoint) => String.fromCodePoint(parseInt(codePoint, 16)));
 }
